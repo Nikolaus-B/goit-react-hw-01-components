dfa06d407ece93c3c1c545bdf057aeefffa0f20c
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-10-28 22:40:11 +0300

    Update Statistic.jsx

--- src/components/Satistic/Statistic.jsx
+++ src/components/Satistic/Statistic.jsx
@@ -8,36 +8,19 @@ import {
 } from './Statystic.styled';
 
 export const Statistic = ({ title, stats }) => {
-  if (title) {
-    return (
-      <StatisticSection>
-        <StatysticText className="title">{title}</StatysticText>
-        <StatysticListContainer>
-          {stats.map(({ id, label, percentage }) => {
-            return (
-              <StatiscticItem key={id} $label={label}>
-                <StatisticLabel>{label}</StatisticLabel>
-                <StatisticPercentage>{percentage}%</StatisticPercentage>
-              </StatiscticItem>
-            );
-          })}
-        </StatysticListContainer>
-      </StatisticSection>
-    );
-  } else {
-    return (
-      <StatisticSection>
-        <StatysticListContainer>
-          {stats.map(({ id, label, percentage }) => {
-            return (
-              <StatiscticItem key={id} $label={label}>
-                <StatisticLabel>{label}</StatisticLabel>
-                <StatisticPercentage>{percentage}%</StatisticPercentage>
-              </StatiscticItem>
-            );
-          })}
-        </StatysticListContainer>
-      </StatisticSection>
-    );
-  }
+  return (
+    <StatisticSection>
+      {title && <StatysticText className="title">{title}</StatysticText>}
+      <StatysticListContainer>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <StatiscticItem key={id} $label={label}>
+              <StatisticLabel>{label}</StatisticLabel>
+              <StatisticPercentage>{percentage}%</StatisticPercentage>
+            </StatiscticItem>
+          );
+        })}
+      </StatysticListContainer>
+    </StatisticSection>
+  );
 };
